f012f917c70919a4e95565ee20902fb9113ed406
Adish Ansari <adishansari786@gmail.com>
2022-05-15 08:30:46 -0700

    Update ShoppingService.java

--- src/main/java/com/common/retail/service/ShoppingService.java
+++ src/main/java/com/common/retail/service/ShoppingService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.common.retail.model.Shopping;
 import com.common.retail.repository.ShoppingRepo;
 
@@ -38,7 +37,7 @@ public class ShoppingService {
         repository.deleteById(id);
         return "Shopping removed !! " + id;
     }
-
+/*
     public Shopping updateShopping(Shopping shopping) {
     	Shopping existingShopping = repository.findById(shopping.getId()).orElse(null);
     	
@@ -48,6 +47,41 @@ public class ShoppingService {
     	existingShopping.setTotalshopping(shopping.getTotalshopping());
         return repository.save(existingShopping);
     }
+*/
+    public Shopping updateShopping(Shopping shopping) {
+    	Shopping outShopping = null;
 
+        if (shopping.getShoppingname() != null && shopping.getShoppingtype() != null) {
+        	Shopping existingShopping = repository.findById(shopping.getId()).orElse(null);
+        	existingShopping.setShoppingname(shopping.getShoppingname());
+        	existingShopping.setShoppingtype(shopping.getShoppingtype());
+        	outShopping = repository.save(existingShopping);
+        } else if (shopping.getShoppingtype() != null && shopping.getTotalshopping() > 0) {
+        	Shopping existingShopping = repository.findById(shopping.getId()).orElse(null);
+        	existingShopping.setShoppingtype(shopping.getShoppingtype());
+            existingShopping.setTotalshopping(shopping.getTotalshopping());
+            outShopping = repository.save(existingShopping);
+        }else if (shopping.getShoppingname() != null && shopping.getTotalshopping() > 0) {
+        	Shopping existingShopping = repository.findById(shopping.getId()).orElse(null);
+        	existingShopping.setShoppingname(shopping.getShoppingname());
+        	existingShopping.setTotalshopping(shopping.getTotalshopping());
+        	outShopping = repository.save(existingShopping);
+        }  else if (shopping.getShoppingtype() != null) {
+        	Shopping existingShopping = repository.findById(shopping.getId()).orElse(null);
+        	existingShopping.setShoppingtype(shopping.getShoppingtype());
+        	outShopping = repository.save(existingShopping);
+        }   else if (shopping.getShoppingname() != null) {
+        	Shopping existingShopping = repository.findById(shopping.getId()).orElse(null);
+        	existingShopping.setShoppingname(shopping.getShoppingname());
+        	outShopping = repository.save(existingShopping);
+        } else if (shopping.getTotalshopping() != 0.0) {
+        	Shopping existingShopping = repository.findById(shopping.getId()).orElse(null);
+        	existingShopping.setTotalshopping(shopping.getTotalshopping());
+        	outShopping = repository.save(existingShopping);
+        }
+        return outShopping;
+    }
+    
+    
 
 }
